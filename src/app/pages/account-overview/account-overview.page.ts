d19dfcc511ef38fa52c17604285ed0a5122c06bc
dhruvesh9 <dun814@gmail.com>
2023-06-29 02:37:02 +0530

    android plus necessary changes

--- src/app/pages/account-overview/account-overview.page.ts
+++ src/app/pages/account-overview/account-overview.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
 import { Toast } from '@capacitor/toast';
+import { BiometryType, NativeBiometric } from "capacitor-native-biometric";
 
 @Component({
   selector: 'app-account-overview',
@@ -17,8 +18,12 @@ export class AccountOverviewPage implements OnInit {
   }
 
   ngOnInit() {
-
-   }
+    const tabBar = window.document.getElementById('tab-bar-id');
+    if (tabBar) {
+      tabBar.style.removeProperty('display');
+    }
+    // this.performBiometricVerificatin();
+  }
 
   async doTopUp() {
     this.showToastMessages('Topup Successful')
@@ -30,11 +35,43 @@ export class AccountOverviewPage implements OnInit {
     await Haptics.vibrate();
   }
 
-  async showToastMessages(message: string){
+  async showToastMessages(message: string) {
     await Toast.show({
       text: message
     });
   }
 
-  
+  async performBiometricVerificatin() {
+    try {
+      const result = await NativeBiometric.isAvailable();
+
+      if (!result.isAvailable) return;
+
+      const isFaceID = result.biometryType == BiometryType.FACE_ID;
+
+      const verified = await NativeBiometric.verifyIdentity({
+        reason: "For easy log in",
+        title: "Log in",
+        subtitle: "Maybe add subtitle here?",
+        description: "Maybe a description too?",
+      })
+        .then(() => true)
+        .catch(() => false);
+
+      if (!verified) return;
+      this.showToastMessages('Is this verified? ' + verified)
+      const credentials = await NativeBiometric.getCredentials({
+        server: "www.example.com",
+      });
+      this.showToastMessages('credentials ' + credentials);
+    } catch (err) {
+      if (err instanceof Error) {
+        this.showToastMessages('Catch error ' + err.message);
+        console.error(err.message);
+      } else {
+        console.error('An unknown error occurred.');
+      }
+      console.log('Biometric not available');
+    }
+  }
 }
